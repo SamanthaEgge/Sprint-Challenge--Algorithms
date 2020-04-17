5dbec21226a1e3840c0106d0b68b6ef3ba201ef1
Samantha Egge <samantha.c.egge@gmail.com>
2020-04-17 08:53:50 -0700

    Completed Count challenge. Messy answer, but passes tests.

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,28 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+count = 0
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+  # This is the exit case, and if there is only 1 or 0 letters, you can't have 'th'
+  global count
+  if len(word) <= 1:
+    # These variables are because I couldn't figure out how to reset counter between function initialization :(
+        ## (but it wooorrkkksss)
+    final = count
+    count = 0
+    # print('final')
+    # print(final)
+    return final
+
+  # Going to begin at the end of the string, and move towards the front checking each pairs of letters for th.
+  # For each set of letters checked, remove the last letter, and move back through count_th until exit case is met
+  # ex. "checkers" check 'rs', remove s, check 'er', remove r, check 'ek', etc etc until len(c) <= 1
+  if word[-2:] == 'th':
+    count += 1
+    # the following two lines need to be refactored :(
+    word = word[:-2:]
+    return count_th(word)
+  else:
+    word = word[:-1:]
+    return count_th(word)
